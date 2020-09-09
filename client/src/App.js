4e5c8520c2524ba42749a3548567698b057a4bbd
Itzamna Huerta <_@itzamna-huerta.local>
2020-09-09 09:56:02 -0400

    -

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Home from './components/Home';
+import Cameras from './components/Cameras';
+import Lens from './components/Lens';
+import Tripods from './components/Tripods'
+import Footer from './components/Footer';
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/cameras">Cameras</Link>
+            </li>
+            <li>
+              <Link to="/lens">Lens</Link>
+            </li>
+            <li>
+              <Link to="/tripods">Tripods</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/cameras">
+            <Cameras />
+          </Route>
+
+          <Route path="/lens">
+            <Lens />
+          </Route>
+
+           <Route path="/tripods">
+            <Tripods />
+          </Route> 
+
+          <Route path="/">
+            <Home />
+          </Route> 
+        </Switch>
+      </div>
+    </Router>
+
+
+    <Footer />
     </div>
   );
 }
